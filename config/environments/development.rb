1ad33c651c78e95e442903a75f6ae52e6f672e52
EC2 Default User <kazukipump7@gmail.com>
2022-07-14 10:08:42 +0000

    Add Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "b6d1e8e6bb6042758f40c5fabb87c1e8.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
